ebc8aedce642d310c5d834de79fc57173261a264
Mateus Alves Conceição <mateus.alvesc02@gmail.com>
2024-06-06 17:08:34 -0300

    events stream

--- src/main/java/com/apirest/webflux/controller/PlaylistController.java
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -3,11 +3,15 @@ package com.apirest.webflux.controller;
 import com.apirest.webflux.document.Playlist;
 import com.apirest.webflux.services.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
-//@RestController
+import java.time.Duration;
+
+@RestController
 public class PlaylistController {
 
     @Autowired
@@ -26,6 +30,15 @@ public class PlaylistController {
     @PostMapping(value = "/playlist")
     public Mono<Playlist> save(@RequestBody Playlist playlist) {
         return service.save(playlist);
+    }
+
+    @GetMapping(value = "/playlist/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> getPlaylistByEvents() {
+
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+        Flux<Playlist> events = service.findAll();
+        System.out.println("Passou aqui events");
+        return Flux.zip(interval, events);
     }
 
 }
